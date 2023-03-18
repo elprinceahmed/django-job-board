12f258d82b6caa5467bfcdefa9bcde85db81b2d3
elprinceahmed <elprinceahmed80@gmail.com>
2023-03-18 10:28:45 +0200

    add job templates

--- job/views.py
+++ job/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
 
 
 def job_list(request):
-    pass
+    job_list = Job.objects.all()
+    # print(job_list)
+    context = {'jobs': job_list}    # template name
+    return render(request, 'job/job_list.html', context)
 
 
 def job_detail(request, id):
-    pass
+    job_detail = Job.objects.get(id=id)
+    context = {'job': job_detail}
+    return render(request, 'job/job_detail.html', context)
