399540d26bb6c12e6180a6e2b8e8faaab01dbcba
elprinceahmed <elprinceahmed80@gmail.com>
2023-03-26 17:29:49 +0200

    upload image

--- job/models.py
+++ job/models.py
@@ -8,6 +8,12 @@ JOB_TYPE = (
 )
 
 
+def image_upload(instance, filename):
+    imagename, extension = filename.split(".")
+    return "jobs/%s.%s" % (instance.id, extension)
+    # return "jobs/%s/%s.%s" % (instance.id, instance.id, extension)
+
+
 class Job(models.Model):    # table
     title = models.CharField(max_length=100)    # column
     # location
@@ -18,6 +24,7 @@ class Job(models.Model):    # table
     salary = models.IntegerField(default=0)
     experinse = models.IntegerField(default=1)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=image_upload)
 
     def __str__(self):
         return self.title
