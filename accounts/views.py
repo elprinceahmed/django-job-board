fbe87fe549be01191524e5bff712729a93df3cd8
elprinceahmed <elprinceahmed80@gmail.com>
2023-04-02 21:43:55 +0200

    edit profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
-from .forms import SignupForm
+from django.urls import reverse
+from .forms import SignupForm, UserForm, ProfileForm
 from django.contrib.auth import authenticate, login
 from .models import Profile
 # Create your views here.
 
 
 def signup(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
@@ -26,5 +27,20 @@ def profile(request):
 
 
 def profile_edit(request):
-    pass
-    # return render(request, 'accounts/profile_edit.html', {})
+    profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        userform = UserForm(request.POST, instance=request.user)
+        profileform = ProfileForm(
+            request.POST, request.FILES, instance=profile)
+        if userform.is_valid() and profileform.is_valid():
+            userform.save()
+            myprofile = profileform.save(commit=False)
+            myprofile.user = request.user
+            myprofile.save()
+            return redirect(reverse('accounts:profile'))
+    else:
+        userform = UserForm(instance=request.user)
+        profileform = ProfileForm(instance=profile)
+
+    return render(request, 'accounts/profile_edit.html', {'userform': userform, 'profileform': profileform})
