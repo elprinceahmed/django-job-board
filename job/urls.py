2e4c70a2925ef262aaea60527dc1d590ec1faca5
elprinceahmed <elprinceahmed80@gmail.com>
2023-04-15 22:01:30 +0200

    function class based view

--- job/urls.py
+++ job/urls.py
@@ -10,5 +10,11 @@ urlpatterns = [
     path('<str:slug>', views.job_detail, name='job_detail'),
 
     # api
-    path('api/list', api.joblistapi, name='joblistapi'),
+    path('api/jobs', api.job_list_api, name='job_list_api'),
+    path('api/jobs/<int:id>', api.job_detail_api, name='job_detail_api'),
+
+    # class based views
+    path('api/v2/jobs', api.JobListApi.as_view(), name='job_list_api'),
+    path('api/v2/jobs/<int:id>', api.JobDetailApi.as_view(), name='job_detail_api'),
+
 ]
